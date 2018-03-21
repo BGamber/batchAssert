5a78f39d2e4fe79efcba2ef21f6eb34c334a1569
Ben Gamber <ben@stickfishstudio.com>
2018-03-21 14:35:35 -0400

    Add main module file, gitignore

---
+++ batchAssert.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+
+let MissingArgumentError = new Error("One or more arguments missing from test object");
+
+let assertTest = function ({ test, result, message }, index) {
+  if (test === undefined ||
+    result === undefined ||
+    message === undefined) {
+    throw MissingArgumentError;
+  }
+  assert.deepEqual(test, result, message);
+  console.log(`Test ${index} passed.`);
+}
+
+let testAll = function (batch, throwOnFail = true) {
+  if (throwOnFail) {
+    batch.forEach((test, index) => { assertTest(test, index) });
+  } else {
+    batch.forEach((test, index) => {
+      try {
+        assertTest(test, index);
+      } catch (AssertionError) {
+        console.log(`Test ${index} FAILED: ${test.message}`)
+      }
+    });
+  };
+};
+
+module.exports.testAll = testAll;
